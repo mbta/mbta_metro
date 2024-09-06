f6886c41f23758c3b50ccee9e56c4a5eae97143d
Anthony Shull <shull.anthony@gmail.com>
2024-09-06 08:46:24 -0500

    add click event

--- lib/blocks/components/button.ex
+++ lib/blocks/components/button.ex
@@ -3,11 +3,14 @@ defmodule Blocks.Components.Button do
 
   use Phoenix.Component
 
+  attr :rest, :global, include: ~w(disabled form name value)
   slot :inner_block, required: true
 
   def button(assigns) do
     ~H"""
-    <button class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
+    <button
+      class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+      {@rest}>
       <%= render_slot(@inner_block) %>
     </button>
     """
--- lib/blocks_web/live/demo.ex
+++ lib/blocks_web/live/demo.ex
@@ -1,3 +1,11 @@
 defmodule BlocksWeb.Live.Demo do
   use Phoenix.LiveView, layout: {BlocksWeb.Layouts, :root}
+
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, show: true)}
+  end
+
+  def handle_event("click", _, socket) do
+    {:noreply, assign(socket, show: false)}
+  end
 end
