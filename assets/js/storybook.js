a266559b5bfbb3305c42b84c42b5c61e8d7db3b0
Anthony Shull <shull.anthony@gmail.com>
2024-09-24 09:30:46 -0500

    remove config for flatpickr

--- assets/js/storybook.js
+++ assets/js/storybook.js
@@ -17,26 +17,6 @@ const i18nDate = (date, locale = navigator.language) => {
   return formatter.format(date);
 }
 
-const config = {
-  allowInvalidPreload: true, // needed on mobile to prevent the input from becoming blank when selecting a date outside the min/max
-  altInput: true, // allow different format to be sent to server
-  dateFormat: SERVER_FORMAT, // this gets sent to the server
-  // defaultDate: dateTime,
-  enableTime: true,
-  // maxDate,
-  // minDate,
-  formatDate: (date, formatString, locale) => {
-    if (formatString === SERVER_FORMAT) {
-      // Formats a date into a string in the format util.ex parse/1 expects.
-      return format(date, "yyyy-MM-dd HH:mm aa");
-    }
-
-    // if not being sent to the server, use localized format
-    return i18nDate(date, locale);
-  },
-  // wrap: true // works with adjacent icon
-}
-
 (function () {
   window.storybook = {
     Hooks
