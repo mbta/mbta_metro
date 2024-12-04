54af9c9df239dcbd004dc9a1853ce767e1c2ccbd
Anthony Shull <shull.anthony@gmail.com>
2024-12-04 14:23:10 -0600

    Update lib/mbta_metro/components/list.ex
    Co-authored-by: Cristen Jones <cjones3@mbta.com>

--- lib/mbta_metro/components/list.ex
+++ lib/mbta_metro/components/list.ex
@@ -21,7 +21,7 @@ defmodule MbtaMetro.Components.List do
   def list(assigns) do
     ~H"""
     <ul class={"max-w-xs flex flex-col divide-y divide-slate-200 #{@class}"}>
-      <li :for={item <- @item} class="inline-flex items-center gap-x-2 py-2 text-sm font-medium text-slate-800">
+      <li :for={item <- @item} class="inline-flex items-center gap-x-2 py-2">
         <%= render_slot(item) %>
       </li>
     </ul>
