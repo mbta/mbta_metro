6999473274a86452207fada0a3ae001edeb9c75d
Anthony Shull <shull.anthony@gmail.com>
2024-12-04 12:44:08 -0600

    add a new list component

---
+++ lib/mbta_metro/components/list.ex
@@ -0,0 +1,30 @@
+defmodule MbtaMetro.Components.List do
+  @moduledoc false
+
+  use Phoenix.Component
+
+  @doc """
+  Renders a list of items.
+
+  ## Examples
+
+      <.list>
+        <:item>Foo</:item>
+        <:item>Bar</:item>
+        <:item>Baz</:item>
+      </.list>
+  """
+  attr :class, :string, default: ""
+
+  slot :item, required: true
+
+  def list(assigns) do
+    ~H"""
+    <ul class={"max-w-xs flex flex-col divide-y divide-slate-200 #{@class}"}>
+      <li :for={item <- @item} class="inline-flex items-center gap-x-2 py-2 text-sm font-medium text-slate-800">
+        <%= render_slot(item) %>
+      </li>
+    </ul>
+    """
+  end
+end
