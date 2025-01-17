2f24bff83203131b1c69b1f0dfc6c12132a40ca2
Cristen Jones <cjones3@mbta.com>
2025-01-17 18:02:11 -0500

    feat: more resilient checkbox/radio focus styles

--- assets/js/plugins/base.js
+++ assets/js/plugins/base.js
@@ -4,11 +4,27 @@ import plugin from "tailwindcss/plugin";
  * Basic styles, CSS resets, etc. for improving cross-browser consistency for
  * the MBTA Metro components
  */
-const basePlugin = plugin(({ addBase }) =>
+const basePlugin = plugin(({ addBase, theme }) =>
   addBase({
     // remove default caret in Safari
     "details > summary::-webkit-details-marker": {
       display: "none"
+    },
+    // the styles from the @tailwindcss/forms plugin aren't customizable, (see
+    // https://github.com/tailwindlabs/tailwindcss/discussions/4645 for
+    // discussion on custom ring color), yet are also too specific and can
+    // override other classes... so we override them as suggested by
+    // https://github.com/tailwindlabs/tailwindcss/discussions/4649#discussioncomment-8714473
+    // to more easily achieve our desired custom focus styles
+    "[type='checkbox'], [type='radio']": {
+      "border-color": theme("colors.cobalt.30"),
+      color: theme("colors.cobalt.30")
+    },
+    "[type='checkbox']:focus, [type='radio']:focus": {
+      "--tw-ring-color": theme("colors.cobalt.60"),
+      "--tw-ring-offset-color": "transparent",
+      "--tw-ring-offset-width": "0px",
+      "border-color": theme("colors.cobalt.30")
     }
   })
 );
