12ce29b5880098e8918e348d2d5f24fcab9781b7
Benji Mauer <benji@heybenji.co>
2025-12-16 14:28:09 -0500

    Add documentation for Form Input and Input Group
    - Fix issue where "Attributes" heading was showing up at the top of the Form Input page
    - Variant-level documentation for both Form Input and Input Group components

--- storybook/components/inputgroup.story.exs
+++ storybook/components/inputgroup.story.exs
@@ -51,7 +51,20 @@ defmodule Storybook.Components.InputGroup do
           Wonderland
         </:input>
         """
-      ]
+      ],
+      note: """
+      Allows users to select 1 or more options from a list.
+
+      ### When to use
+
+      * **There is 1 or more valid responses.** When a user can select any number of options from a list.
+
+      * **Each option is a yes/no or true/false answer.**
+
+      ### When to consider something else
+
+      * **Only one response is wanted.** If only one response is desired, such as a single choice on a scale, consider `radio` or `select`.
+      """
     }
   end
 
@@ -85,7 +98,18 @@ defmodule Storybook.Components.InputGroup do
           Wonderland
         </:input>
         """
-      ]
+      ],
+      note: """
+      Allows users to select 1 or more options from a list, styled as horizontal segmented buttons.
+
+      ### When to use
+
+      * **Vertical compactness is paramount.** Use sparingly only as a control for other content on the page, such as mode filters.
+
+      ### When to consider something else
+
+      * **Only one response is wanted.** If only one response is desired, such as a single choice on a scale, consider `radio` with the `as_buttons` property.
+      """
     }
   end
 
@@ -117,7 +141,18 @@ defmodule Storybook.Components.InputGroup do
           Wonderland
         </:input>
         """
-      ]
+      ],
+      note: """
+      A pre-set list of options where only 1 option can be selected at a time.
+
+      ### When to use
+
+      * **Asking a multiple choice question that has only 1 answer.** For example: "Choose a direction: Inbound or Outbound"
+
+      ### When to consider something else
+
+      * **There are more than 7 options.** Consider a `select` component instead.
+      """
     }
   end
 
@@ -154,7 +189,18 @@ defmodule Storybook.Components.InputGroup do
         <span>Jackson Square</span><.icon name="circle" class="w-4 h-4 fill-orange-line" />
         </:input>
         """
-      ]
+      ],
+      note: """
+      Allows users to select only 1 one option from a list, styled as horizontal segmented buttons.
+
+      ### When to use
+
+      * **Vertical compactness is paramount.** Use sparingly only as a control for other content on the page, such selecting a timeframe for a planned trip.
+
+      ### When to consider something else
+
+      * **More than one response is needed.** If more than one response is desired, consider `checkbox` with the `as_buttons` property.
+      """
     }
   end
 end
